09d42063495ad44cfbffceb3594636a0614b6816
shivam-jha2712 <shivam.jha.2712@gmail.com>
2024-09-19 17:59:00 +0530

    rust-basics-and-setup

---
+++ rust-basics/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Basic intitalisation command -> "cargo init"
+// Basic intitalisation command for library creation will be  -> "cargo init --lib"
---
+++ whyRust/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 1;
+    // x= "shivam";
+    println!("Hello World!");
+    println!("x: {}", x); // Prints the value of x
+}
