8814bde3cc773800d8bcaa1650c68d29724d8657
shivam-jha2712 <shivam.jha.2712@gmail.com>
2024-09-19 18:00:39 +0530

    commit-comments

--- rust-basics/src/main.rs
+++ rust-basics/src/main.rs
@@ -2,5 +2,5 @@ fn main() {
     println!("Hello, world!");
 }
 
-// Basic intitalisation command -> "cargo init"
+// Basic intitalisation command for end user app setup-> "cargo init"
 // Basic intitalisation command for library creation will be  -> "cargo init --lib"
